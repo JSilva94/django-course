1e440b4bccbd5c961f378ad4c7f2b0cb4f25c823
Jayde Silva <jayde.silva@mail.utoronto.ca>
2019-02-19 18:51:21 -0500

    initializing cbv

--- first_app/views.py
+++ first_app/views.py
@@ -4,7 +4,6 @@ from first_app.models import Topic, WebPage, AccessRecord
 # Create your views here.
 
 def index(req):
-    webpages_list = AccessRecord.objects.order_by('date')
-    date_dict = {'access_records': webpages_list}
+   
 
-    return render(req, 'first_app/index.html', context=date_dict)
+    return render(req, '/index.html')
